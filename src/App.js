1034417a2125a5e612208cd735f05f41cfcd2f77
Joel Cooper <jodycooper16@gmail.com>
2021-05-21 10:52:13 -0700

    commit tree 7e36e14d50a9572d069e168229e4da925a5ee381

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
-// create your App component here
+// create your App component hered
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    sate = {
+        peopleInSpace: []
+    }
+
+    componentDidMount () {
+      fetch('http://api.open-notify.org/astros.json')
+      .then(res => res.json())
+      .then(data=> [
+          this.setState({
+              peopleInSpace: data.people
+          })
+      ]) 
+    }
+
+
+    render() {
+        
+        return (
+            <div>
+              
+            </div>
+        )
+    }
+}
